db43cfea548ea60d040988cabd92091e2b797c6b
Gene Liverman <me@technicalissues.us>
2017-05-07 14:33:35 -0700

    Fixed class name (darn copy/paste)

--- puppet/site/role/manifests/server.pp
+++ puppet/site/role/manifests/server.pp
@@ -2,6 +2,6 @@
 # One big difference between this and the workstation role is that you generally
 # are not standing in front of the system and / or there is no graphical
 # interface.
-class role::workstation {
+class role::server {
   include ::profile::base
 }
