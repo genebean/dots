4b75708af592b774b23920745c90e88c04addb3a
Gene Liverman <gene@technicalissues.us>
2018-02-10 20:16:47 -0800

    Created fuction to parse username from $homedir
    $homedir is pulled from Hiera

---
+++ puppet/production/site/custom_libs/lib/puppet/functions/homedir_to_user.rb
@@ -0,0 +1,10 @@
+Puppet::Functions.create_function(:homedir_to_user) do
+  dispatch :homedir_to_user do
+    param 'String', :some_path
+    return_type 'String'
+  end
+
+  def homedir_to_user(some_path)
+    some_path.split('/')[-1]
+  end
+end
