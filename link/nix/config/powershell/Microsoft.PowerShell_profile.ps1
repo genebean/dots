71c3a8e27a521a9ba1a8be3b98ab3ab6efa2e032
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-05-28 10:40:11 -0400

    Added notes on installing imported modules

--- link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
+++ link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
 # With PowerCLI installed via ps-get this isn't needed anymore
 #Get-Module -ListAvailable PowerCLI.* | Import-Module
 
+# PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
 Import-Module '/Users/gene.liverman/.local/share/powershell/Modules/posh-git/1.0.0/posh-git.psd1'
+
+# Install-Module -Name oh-my-posh
 Import-module oh-my-posh
