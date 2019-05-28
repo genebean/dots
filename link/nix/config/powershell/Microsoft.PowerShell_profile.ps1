d944cb0a530528109d9476170660cec5ae297843
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-05-28 10:23:36 -0400

    Added posh-git to PowerShell profile

--- link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
+++ link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 # With PowerCLI installed via ps-get this isn't needed anymore
 #Get-Module -ListAvailable PowerCLI.* | Import-Module
+
+Import-Module '/Users/gene.liverman/.local/share/powershell/Modules/posh-git/1.0.0/posh-git.psd1'
