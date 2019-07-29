9a99df2bed6083824088a787b1c2e054e2fff2c1
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-07-29 14:05:33 -0400

    Powershell changes

--- link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
+++ link/nix/config/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,10 @@
 #Get-Module -ListAvailable PowerCLI.* | Import-Module
 
 # PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-Import-Module '/Users/gene.liverman/.local/share/powershell/Modules/posh-git/1.0.0/posh-git.psd1'
+#Import-Module '/Users/gene.liverman/.local/share/powershell/Modules/posh-git/1.0.0/posh-git.psd1'
+Import-Module posh-git
 
 # Install-Module -Name oh-my-posh
 Import-module oh-my-posh
+#Set-Theme Paradox
+Set-Theme beantest
