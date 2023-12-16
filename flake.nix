5a7dacbaab382c64447bf5cb3405787fad68399f
Gene Liverman <gene.liverman@ltnglobal.com>
2023-12-16 09:14:06 -0500

    Update code based on upstream changes

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         config = {
           allowUnfree = true;
           permittedInsecurePackages = [
-            "python-2.7.18.6"
+            "python-2.7.18.7"
           ];
         };
       };
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -45,12 +45,15 @@
         theme = "Catppuccin-frappe";
       };
       themes = {
-        Catppuccin-frappe = builtins.readFile (pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        } + "/Catppuccin-frappe.tmTheme");
+        Catppuccin-frappe = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-frappe.tmTheme";
+        };
       };
     };
     eza.enable = true;
