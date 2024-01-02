e0a83fdeb8abe230cef69f3e17738cae943efb74
Gene Liverman <gene.liverman@ltnglobal.com>
2024-01-02 16:06:59 -0500

    Add vim background option, update formatting

--- modules/home-manager/files/nvim/lua/config/keymaps.lua
+++ modules/home-manager/files/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- make sure vim know I always have a dark terminal
+vim.opt.background = "dark"
+
 -- use spaces for tabs and whatnot
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
@@ -8,4 +11,4 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
