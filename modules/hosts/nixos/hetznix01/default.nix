d77634f7f738cea898214a8558d9eb2f21621f67
Gene Liverman <gene.liverman@ltnglobal.com>
2024-02-05 17:03:13 -0500

    Add bootloader and not-detected.nix import

--- modules/hosts/nixos/hetznix01/default.nix
+++ modules/hosts/nixos/hetznix01/default.nix
@@ -4,6 +4,13 @@
     ./disk-config.nix
   ];
 
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
   system.stateVersion = "23.11";
 
   networking = {
--- modules/hosts/nixos/hetznix01/hardware-configuration.nix
+++ modules/hosts/nixos/hetznix01/hardware-configuration.nix
@@ -4,9 +4,10 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
