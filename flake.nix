5704463a416b7871b20b624770843a3b1c4309f0
Gene Liverman <gene@technicalissues.us>
2024-06-05 22:54:03 -0400

    fix typo

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
     home-manager, nix-darwin, nix-flatpak, nix-homebrew, nixos-hardware, nixpkgs-terraform, sops-nix, ... }: let
 
     # creates a macOS system config
-    darwinHostConfig = { system, hostname, username, additionaModules, additionaSpecialArgs }: nix-darwin.lib.darwinSystem {
+    darwinHostConfig = { system, hostname, username, additionalModules, additionalSpecialArgs }: nix-darwin.lib.darwinSystem {
       pkgs = import nixpkgs {
         inherit system;
         config = {
@@ -70,7 +70,7 @@
         };
         overlays = [ nixpkgs-terraform.overlays.default ];
       };
-      specialArgs = { inherit inputs hostname username; } // additionaSpecialArgs;
+      specialArgs = { inherit inputs hostname username; } // additionalSpecialArgs;
       modules = [
         nix-homebrew.darwinModules.nix-homebrew {
           nix-homebrew = {
@@ -94,11 +94,11 @@
 
         ./modules/system/common/all-darwin.nix # system-wide stuff
         ./modules/hosts/darwin/${hostname} # host specific stuff
-      ] ++ additionaModules; # end modules
+      ] ++ additionalModules; # end modules
     }; # end darwinSystem
 
     # creates a nixos system config
-    nixosHostConfig = { system, hostname, username, additionaModules, additionaSpecialArgs }: nixpkgs.lib.nixosSystem {
+    nixosHostConfig = { system, hostname, username, additionalModules, additionalSpecialArgs }: nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs compose2nix hostname username;
         pkgs = import nixpkgs {
           inherit system;
@@ -108,7 +108,7 @@
           };
           overlays = [ nixpkgs-terraform.overlays.default ];
         };
-      } // additionaSpecialArgs;
+      } // additionalSpecialArgs;
       modules = [
         disko.nixosModules.disko
 
@@ -128,10 +128,10 @@
         sops-nix.nixosModules.sops # system wide secrets management
         ./modules/system/common/all-nixos.nix # system-wide stuff
         ./modules/hosts/nixos/${hostname} # host specific stuff
-      ] ++ additionaModules;
+      ] ++ additionalModules;
     }; # end nixosSystem
 
-    linuxHomeConfig = { system, hostname, username, additionaModules, additionaSpecialArgs }: home-manager.lib.homeManagerConfiguration {
+    linuxHomeConfig = { system, hostname, username, additionalModules, additionalSpecialArgs }: home-manager.lib.homeManagerConfiguration {
       extraSpecialArgs = { inherit genebean-omp-themes hostname username;
         pkgs = import nixpkgs {
           inherit system;
@@ -141,7 +141,7 @@
           };
           overlays = [ nixpkgs-terraform.overlays.default ];
         };
-      } // additionaSpecialArgs;
+      } // additionalSpecialArgs;
       modules = [
         ./modules/home-manager/hosts/${hostname}/${username}.nix
         {
@@ -151,7 +151,7 @@
           };
         }
         sops-nix.homeManagerModules.sops
-      ] ++ additionaModules;
+      ] ++ additionalModules;
     }; # end homeManagerConfiguration
 
   in {
@@ -161,22 +161,22 @@
         system = "x86_64-darwin";
         hostname = "AirPuppet";
         username = "gene";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
       Blue-Rock = darwinHostConfig {
         system = "x86_64-darwin";
         hostname = "Blue-Rock";
         username = "gene.liverman";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
       mightymac = darwinHostConfig {
         system = "aarch64-darwin";
         hostname = "mightymac";
         username = "gene.liverman";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
     }; # end darwinConfigurations
 
@@ -186,24 +186,24 @@
         system = "aarch64-linux";
         hostname = "hetznix01";
         username = "gene";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
       nixnuc = nixosHostConfig {
         system = "x86_64-linux";
         hostname = "nixnuc";
         username = "gene";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
       rainbow-planet = nixosHostConfig {
         system = "x86_64-linux";
         hostname = "rainbow-planet";
         username = "gene";
-        additionaModules = [
+        additionalModules = [
           nixos-hardware.nixosModules.dell-xps-13-9360
         ];
-        additionaSpecialArgs = {};
+        additionalSpecialArgs = {};
       };
     }; # end nixosConfigurations
 
@@ -213,8 +213,8 @@
         system = "x86_64-linux";
         hostname = "mini-watcher";
         username = "gene";
-        additionaModules = [];
-        additionaSpecialArgs = {};
+        additionalModules = [];
+        additionalSpecialArgs = {};
       };
     }; # end homeConfigurations
   };
