f0c351cbbe27defa4fa0f907055bfc3ddf9bb75e
Gene Liverman <gene@technicalissues.us>
2024-12-31 15:57:27 -0500

    Cleanup + a few apps

--- modules/hosts/nixos/bigboy/default.nix
+++ modules/hosts/nixos/bigboy/default.nix
@@ -1,10 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ inputs, config, pkgs, username, ... }:
-
-{
+{ inputs, config, pkgs, username, ... }: {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../common/linux/flatpaks.nix
@@ -13,7 +7,6 @@
 
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  # Bootloader.
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
@@ -21,101 +14,30 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    description = "Gene Liverman";
-    extraGroups = [ "networkmanager" "wheel" "dialout" "input" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
+    environment.systemPackages = with pkgs; [
     angryipscanner
     displaylink
+    filezilla
     gitkraken
     kdePackages.ksshaskpass
     libreoffice
     meld
+    mumble
     networkmanager-openvpn
+    rclone
+    rclone-browser
     slack
     tilix
     vivaldi
+    vlc
     xorg.xf86videofbdev
     xfce.xfce4-terminal
     zoom-us
   ];
+
+  hardware.pulseaudio.enable = false;
+  
+  networking.networkmanager.enable = true;
 
   programs = {
     _1password.enable = true;
@@ -126,6 +48,8 @@
       polkitPolicyOwners = [ "${username}" ];
     };
 
+    firefox.enable = true;
+
     ssh.askPassword = "ssh-askpass";
 
     # common programs that really should be in another file
@@ -133,9 +57,30 @@
     xfconf.enable = true;
   };
 
+  security.rtkit.enable = true;
+
   services = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+    desktopManager.plasma6.enable = true;
+    fstrim.enable = true;
     fwupd.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
     printing.enable = true; # Enable CUPS
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
   };
 
   sops = {
@@ -153,4 +98,14 @@
     };
   };
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${username} = {
+    isNormalUser = true;
+    description = "Gene Liverman";
+    extraGroups = [ "networkmanager" "wheel" "dialout" "input" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
 }
--- modules/hosts/nixos/bigboy/hardware-configuration.nix
+++ modules/hosts/nixos/bigboy/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "sg" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
