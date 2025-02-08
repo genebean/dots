b37b7ebf415def97712ba073f1efa2cc8820e683
Gene Liverman <gene.liverman@ltnglobal.com>
2025-02-08 19:48:55 +0100

    Install Facebook Messenger on macOS
    Not my favorite platform by any means, but people talk to me here

--- modules/hosts/darwin/mightymac/default.nix
+++ modules/hosts/darwin/mightymac/default.nix
@@ -66,6 +66,7 @@
       "HomeCam" = 1292995895;
       "Keeper Password Manager" = 414781829;
       "MeetingBar" = 1532419400;
+      "Messenger" = 1480068668;
       "Microsoft Remote Desktop" = 1295203466;
       "Telegram" = 747648890;
       "WhatsApp Messenger" = 310633997;
