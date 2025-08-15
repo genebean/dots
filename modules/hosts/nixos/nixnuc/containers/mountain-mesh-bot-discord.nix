5bb15f31199430b9c20f36369cb5371bc529d31f
Gene Liverman <gene@technicalissues.us>
2025-08-15 08:02:11 -0400

    Add mountain-mesh-bot-discord

---
+++ modules/hosts/nixos/nixnuc/containers/mountain-mesh-bot-discord.nix
@@ -0,0 +1,25 @@
+{ config, username, ... }: let
+  volume_base = "/orico/mountain-mesh-bot-discord";
+in {
+  # My mountain-mesh-bot-discord container
+
+  virtualisation.oci-containers.containers = {
+    "mtnmesh_bot_discord" = {
+      autoStart = true;
+      image = "ghcr.io/genebean/mountain-mesh-bot-discord:main";
+      podman.user = username;
+      pull = "always";
+      volumes = [
+        "${volume_base}/.env:/src/.env"
+      ];
+    };
+  };
+
+  services.restic.backups.daily.paths = [ volume_base ];
+
+  sops.secrets.mtnmesh_bot_dot_env = {
+    owner = "${username}";
+    path = "${volume_base}/.env";
+    restartUnits = [ "${config.virtualisation.oci-containers.containers.mtnmesh_bot_discord.serviceName}" ];
+  };
+}
