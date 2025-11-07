2e7db512ea49bb85b655f9e463ee8506e4dc8f79
Gene Liverman <gene@technicalissues.us>
2025-11-07 08:39:10 -0500

    Update proxy

--- modules/hosts/nixos/hetznix02/post-install/nginx.nix
+++ modules/hosts/nixos/hetznix02/post-install/nginx.nix
@@ -1,8 +1,6 @@
 
-{ config, pkgs, ... }: let
+{ pkgs, ... }: let
   domain = "genebean.me";
-  http_port = 80;
-  https_port = 443;
 in {
   environment.etc.nginx-littlelinks = {
     # Info generated via
@@ -28,7 +26,7 @@ in {
     recommendedBrotliSettings = true;
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
-    recommendedProxySettings = true;
+    #recommendedProxySettings = true;
     recommendedTlsSettings = true;
     appendHttpConfig = ''
       # Add HSTS header with preloading to HTTPS requests.
@@ -47,10 +45,8 @@ in {
         enableACME = true;
         acmeRoot = null;
         forceSSL = true;
+        root = "/etc/nginx-littlelinks";
         locations = {
-          "/" = {
-            root = "/etc/nginx-littlelinks";
-          };
           "/.well-known/lnurlp/genebean" = {
             return = ''
               200 '{"status":"OK","tag":"payRequest","commentAllowed":255,"callback":"https://getalby.com/lnurlp/genebean/callback","metadata":"[[\\"text/identifier\\",\\"genebean@getalby.com\\"],[\\"text/plain\\",\\"Sats for GeneBean\\"]]","minSendable":1000,"maxSendable":10000000000,"payerData":{"name":{"mandatory":false},"email":{"mandatory":false},"pubkey":{"mandatory":false}},"nostrPubkey":"79f00d3f5a19ec806189fcab03c1be4ff81d18ee4f653c88fac41fe03570f432","allowsNostr":true}'
@@ -73,17 +69,12 @@ in {
           };
           "/api/event" = {
             proxyPass = "https://stats.technicalissues.us/api/event";
-            proxyWebsockets = true;
           };
           "/github" = {
             return = "301 https://github.com/genebean";
           };
           "/js/script.hash.outbound-links.js" = {
             proxyPass = "https://stats.technicalissues.us/js/script.hash.outbound-links.js";
-            proxyWebsockets = true;
-            extraConfig = ''
-              add_header Content-Type application/x-javascript;
-            '';
           };
           "/mastodon" = {
             return = "302 https://fosstodon.org/@genebean";
