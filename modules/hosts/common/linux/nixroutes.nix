46c825d7a498e44a6b00aa820c6b5033261abe34
Gene Liverman <gene@technicalissues.us>
2026-03-10 00:41:32 -0400

    alias

--- modules/hosts/common/linux/nixroutes.nix
+++ modules/hosts/common/linux/nixroutes.nix
@@ -3,5 +3,5 @@ let
   hostName = config.networking.hostName;
 in {
   programs.zsh.shellAliases.nixroutes =
-    "cd ~/repos/dots && echo '=== Current Routes ===' && ip route show && ip -6 route show && echo '' && echo '=== New Build Routes ===' && nix eval --apply 'routes: builtins.concatStringsSep \"\\\\n\" (map (r: r.Destination + \" via \" + r.Gateway) routes)' '.#nixosConfigurations.${hostName}.config.systemd.network.networks.\"10-wan\".routes' | tr '\\\\n' '\\n'";
+    "cd ~/repos/dots && echo '=== Current Routes ===' && ip route show && ip -6 route show && echo '' && echo '=== New Build Routes ===' && nix eval --json '.#nixosConfigurations.${hostName}.config.systemd.network.networks.\"10-wan\".routes' | jq -r '.[] | \"\(.Destination) via \(.Gateway)\"'";
 }
