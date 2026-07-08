6ffd4d736b6ad5f9e2a632486a894328aaa0904e
Gene Liverman <gene@technicalissues.us>
2026-07-08 18:02:00 -0400

    Update ABS to 2.35.1

--- modules/hosts/nixos/nixnuc/containers/audiobookshelf.nix
+++ modules/hosts/nixos/nixnuc/containers/audiobookshelf.nix
@@ -8,7 +8,7 @@ in
   virtualisation.oci-containers.containers = {
     "audiobookshelf" = {
       autoStart = true;
-      image = "ghcr.io/advplyr/audiobookshelf:2.35.0";
+      image = "ghcr.io/advplyr/audiobookshelf:2.35.1";
       environment = {
         AUDIOBOOKSHELF_UID = "99";
         AUDIOBOOKSHELF_GID = "100";
